27431fb2c12066ba86018dd269fdd11d483e7ac3
swatinavghare04 <75429629+swatinavghare04@users.noreply.github.com>
2020-12-28 15:26:19 +0530

    c program to print the result of dividing two nums

---
+++ THIRD.C
@@ -0,0 +1,15 @@
+//wa C program to print the result of dividing two numbers;
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,Div;
+clrscr();
+printf("enter two numbers foe division :");
+scanf("%d%d",&a,&b);
+Div=a/b;
+printf("division of two  numbers is : %d",Div);
+getch();
+
+}
