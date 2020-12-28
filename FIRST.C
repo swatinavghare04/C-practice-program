2a80eb358e7793afe127858fb5fb18db4978914a
swatinavghare04 <75429629+swatinavghare04@users.noreply.github.com>
2020-12-28 15:23:57 +0530

    C program to print Hello

---
+++ FIRST.C
@@ -0,0 +1,12 @@
+// write a c program to print hellow and your name in a seperate line;
+
+#include<stdio.h>
+#include<conio.h>
+void main();
+{
+
+printf("\nhello");
+printf("swati");
+getch();
+
+}
