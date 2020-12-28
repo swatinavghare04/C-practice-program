cf99a59be3bedeaa4dab56cefdef133f55e2122e
swatinavghare04 <75429629+swatinavghare04@users.noreply.github.com>
2020-12-28 15:24:56 +0530

    C program to print sum of two numbers

---
+++ SECOND.C
@@ -0,0 +1,15 @@
+//wa C program to print the result of dividing two numbers;
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,Sum;
+clrscr();
+printf("enter two numbers foe addition :");
+scanf("%d%d",&a,&b);
+Sum=a+b;
+printf("sum of two  numbers is : %d",Sum);
+getch();
+
+}
