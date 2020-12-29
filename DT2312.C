46a7a68f25dad94e1e012e0dc5f45ad1700e0e4d
swatinavghare04 <75429629+swatinavghare04@users.noreply.github.com>
2020-12-29 15:31:23 +0530

    c program to read any month no & disply month nam

---
+++ DT2312.C
@@ -0,0 +1,41 @@
+// wap to read any month number in integer and display the number of days for this month;
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+clrscr();
+printf("enter any month number : ");
+scanf("%d",&n);
+switch(n)
+{
+case 1: printf("January");
+	  break;
+case 2: printf("Febuary");
+	  break;
+case 3: printf("March");
+	  break;
+case 4: printf("April");
+	  break;
+case 5: printf("May");
+	  break;
+case 6: printf("June");
+	  break;
+case 7: printf("July");
+	  break;
+case 8: printf("August");
+	  break;
+case 9: printf("September");
+	  break;
+case 10: printf("October");
+	  break;
+case 11: printf("November");
+	 break;
+case 12: printf("December");
+	 break;
+default : printf("Null");
+	  break;
+}
+getch();
+}
