93c785b37bb347fe808ce170ccb1d111f1f3fdb3
swatinavghare04 <75429629+swatinavghare04@users.noreply.github.com>
2020-12-30 18:37:33 +0530

    c program to find the addition of two matrix

---
+++ DT3005.C
@@ -0,0 +1,66 @@
+
+// wap for addition of two matrix;
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int m1[2][2];
+int m2[2][2];
+int add[2][2],row,col;
+clrscr();
+printf("Input the elments of first matrix :\n");
+for(row=0;row<2;row++)
+{
+   for(col=0;col<2;col++)
+   {
+     scanf("%d",&m1[row][col]);
+   }
+
+}
+printf("Input the element of second matrix :\n");
+for(row=0;row<2;row++)
+{
+   for(col=0;col<2;col++)
+   {
+     scanf("%d",&m2[row][col]);
+   }
+
+}
+ printf("The first matrix is :\n");
+for(row=0;row<2;row++)
+{
+    for(col=0;col<2;col++)
+	{
+      printf("%d\t",m1[row][col]);
+     }
+    printf("\n");
+}
+printf("The second matrix is :\n");
+for(row=0;row<2;row++)
+{
+    for(col=0;col<2;col++)
+	{
+      printf("%d\t",m2[row][col]);
+     }
+    printf("\n");
+}
+for(row=0;row<2;row++)
+{
+for(col=0;col<2;col++)
+{
+add[row][col] = m1[row][col]+m2[row][col];
+}
+}
+printf("Addition of two matrix is :\n");
+for(row=0;row<2;row++)
+{
+    for(col=0;col<2;col++)
+	{
+      printf("%d\t",add[row][col]);
+     }
+    printf("\n");
+}
+
+getch();
+}
