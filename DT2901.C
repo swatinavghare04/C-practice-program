d37e3f70cd5baa496db52092908a2dfeae92f9ba
swatinavghare04 <75429629+swatinavghare04@users.noreply.github.com>
2020-12-30 18:24:23 +0530

    c program to store elements in an array

---
+++ DT2901.C
@@ -0,0 +1,18 @@
+// wap to store element in an array and pront it;
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int e[3]={11,22,33};
+int i;
+clrscr();
+printf("element in an array are :\n");
+for(i=0;i<3;i++)
+{
+
+printf("%d\n",e[i]);
+
+}
+getch();
+}
